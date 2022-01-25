6eeda26163181c2e72cd4a826bb9fea9ea1ff3c7
hyesunie <hyesunie1025@gmail.com>
2022-01-25 23:58:35 +0900

    feat: kakao map definition 파일 생성, map-option 클래스 구현

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,13 @@
 import Router from "./router";
 import Action from "./component/state/action";
 import UserInfo from "./component/state/user-info";
+import MapOption from "./component/state/map-option";
 
 function main() {
   const action = new Action();
   const userInfo = new UserInfo(action);
+  const mapOption = new MapOption(action);
+  mapOption.init();
   userInfo.init();
   new Router(action);
 }
